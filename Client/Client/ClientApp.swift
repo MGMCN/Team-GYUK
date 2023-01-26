1f50cd5448d4a8a08c18a865cfd9038bce32db55
gaoshan <takayamachina@gmail.com>
2023-01-26 12:30:48 +0900

    add client proj

---
+++ Client/Client/ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClientApp.swift
+//  Client
+//
+//  Created by 高山 on 2023/1/26.
+//
+
+import SwiftUI
+
+@main
+struct ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
