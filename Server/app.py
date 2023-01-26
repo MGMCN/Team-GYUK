a7a0a904d4dcf8491ecff734e38a6378f4045cfa
WEI44ZHEYUAN <yuenwei44@gmail.com>
2023-01-26 19:38:21 +0900

    distribute the backend work

--- Server/app.py
+++ Server/app.py
@@ -1,12 +1,15 @@
 from flask import Flask
+from auth import authbp
+from bksf import bksfbp
 
 app = Flask(__name__)
+app.register_blueprint(authbp)
+app.register_blueprint(bksfbp)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
-
 
 if __name__ == '__main__':
     app.run()
---
+++ Server/bksf.py
@@ -0,0 +1,4 @@
+from flask import Blueprint
+
+bksfbp = Blueprint('bksfbp',__name__)
+
