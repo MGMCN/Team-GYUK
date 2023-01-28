6f633994683d565dd8a2cf8aeecbd8392c07e8f8
SK_Kazuki <sendakazuki6@gmail.com>
2023-01-29 03:43:17 +0900

    #12 feat: add models module!

---
+++ Server/models.py
@@ -0,0 +1,8 @@
+from Server import db
+
+class Book(db.Model):
+    id = db.Column('bookId', db.Integer, primary_key = True)
+    bookName = db.Column(db.String(255))
+    bookStatus = db.Column(db.String(255))
+    def __repr__(self):
+        return '<Book {}>'.format(self.bookName)
