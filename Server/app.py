c971b294b11be6d5ea82b0ecccb42313c0ab9266
WEI44ZHEYUAN <yuenwei44@gmail.com>
2023-01-28 13:48:29 +0900

    new feature register

--- Server/app.py
+++ Server/app.py
@@ -1,15 +1,27 @@
 from flask import Flask
 from auth import authbp
 from bksf import bksfbp
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.register_blueprint(authbp)
 app.register_blueprint(bksfbp)
 
 
+#MySQL Connection
+app.config['MYSQL_HOST'] = '163.221.29.193'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_DB'] = 'bookmanagement'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app) 
+
+
 @app.route('/')
 def hello_world():  # put application's code here
+   
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
