3452349c7a18310ab1a5ed80122bde71fdfaf2bb
SK_Kazuki <sendakazuki6@gmail.com>
2023-02-04 02:19:22 +0900

    #12 feat: add database_name in config module:)

--- Server/config.py
+++ Server/config.py
@@ -4,11 +4,12 @@ import os
 load_dotenv()
 
 DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_NAME = os.getenv('DATABASE_NAME')
 USER = os.getenv('USER')
 PASSWORD = os.getenv('PASSWORD')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = f'mysql://{USER}:{PASSWORD}@{DATABASE_URL}/bookmanagement'
+    SQLALCHEMY_DATABASE_URI = f'mysql://{USER}:{PASSWORD}@{DATABASE_URL}/{DATABASE_NAME}'
     SECRET_KEY = SECRET_KEY
     ITEMS_PER_PAGE = 5
