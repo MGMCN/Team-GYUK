5fee65919356d0580d30c57ffc8900582fd5c44a
WEI44ZHEYUAN <yuenwei44@gmail.com>
2023-02-06 14:14:29 +0900

    Update

--- Server/app.py
+++ Server/app.py
@@ -18,10 +18,47 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app) 
 
 
+
+
+
 @app.route('/')
 def hello_world():  # put application's code here
-   
-    return 'Hello World!'
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute(''' CREATE TABLE `Serverdb`.`user` (
+            `userId` INT NOT NULL AUTO_INCREMENT,
+            `name` VARCHAR(255) NOT NULL,
+            `email` VARCHAR(255) NOT NULL,
+            `pwd` VARCHAR(255) NOT NULL,
+            `authType` VARCHAR(255) NOT NULL,
+            PRIMARY KEY (`userId`),
+            UNIQUE INDEX `userId_UNIQUE` (`userId` ASC),
+            UNIQUE INDEX `email_UNIQUE` (`email` ASC));
+            ''')
+        cur.execute(''' CREATE TABLE `book` (
+            `bookName` varchar(255) NOT NULL,
+            `bookId` int(255) NOT NULL,
+            `bookStatus` varchar(255) NOT NULL,
+            PRIMARY KEY (`bookId`),
+            UNIQUE KEY `unique_bookId` (`bookId`)
+            );
+            ''')
+        cur.execute(''' CREATE TABLE `borrowStatus` (
+            `borrowId` int(255) NOT NULL,
+            `userId` int(255) NOT NULL,
+            `bookId` int(255) NOT NULL,
+            PRIMARY KEY (`borrowId`),
+            UNIQUE KEY `unique_borrowId` (`borrowId`),
+            KEY `lnk_user_borrowStatus` (`userId`),
+            KEY `lnk_book_borrowStatus` (`bookId`),
+            CONSTRAINT `lnk_book_borrowStatus` FOREIGN KEY (`bookId`) REFERENCES `book` (`bookId`) ON DELETE CASCADE ON UPDATE CASCADE,
+            CONSTRAINT `lnk_user_borrowStatus` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE
+            );
+            ''')
+        mysql.connection.commit()
+    except cur.OperationalError:
+
+        return 'Hello World!'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
--- Server/auth.py
+++ Server/auth.py
@@ -13,46 +13,26 @@ mysql = MySQL(authapp)
 #register (parameter: email, password, name, authorityType)
 @authbp.route('/register', methods=["POST"])
 def register():
+    try:
     # retrieve the data from the request
-    email = request.form["email"]
-    password = request.form["password"]
-    name = request.form["name"]
-    authorityType = request.form["authorityType"]
-    # declare the error message as none
-    error = None
-    # check the element is empty or not 
-    if not email:
-        error = "Email is required."
-        return error 
-    elif not password:
-        error = "Password is required."
-        return error
-    elif not name:
-        error = "name is required."
-        return error
-    # checking the duplicate or the account and store in the datebase
-    if error is None:
+        email = request.form["email"]
+        password = request.form["password"]
+        name = request.form["name"]
+        authorityType = request.form["authorityType"]
+        # checking the duplicate or the account and store in the datebase
         try:
             # extract the email data from the database 
             cur = mysql.connection.cursor()
-            cur.execute('''SELECT email FROM user''')
-            email_list = []
-            for i in cur.fetchall():
-                email_list.append(i['email'])
-            
-            if email in email_list:
-                return "Email is already registered."
-            else:
-                # save new account into the database
-                cur.execute("INSERT INTO user(userId,name,email,pwd,authType) VALUES (%s,%s,%s,%s,%s)",(0,name,email,generate_password_hash(password),authorityType))      
-                mysql.connection.commit()
-
+            cur.execute("INSERT IGNORE INTO user(userId,name,email,pwd,authType) VALUES (%s,%s,%s,%s,%s)",(0,name,email,generate_password_hash(password),authorityType))      
+            mysql.connection.commit()
         except cur.IntegrityError:
-            error = f"User {email} is already registered."
+            return f"missing some value"
         else:
             # Success, go to the login page.
             return 'Success'
-    flash(error)
+    except KeyError:
+        # if POST miss some value
+        return "Miss Some value"
 
 
 #login  (parameter: email, password)
