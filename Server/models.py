f2fd8bbdb6f40dac99322b360003cc8a921ea77c
SK_Kazuki <sendakazuki6@gmail.com>
2023-02-07 00:52:35 +0900

    #12 fix: delete models.py

--- Server/models.py
+++
@@ -1,8 +0,0 @@
-from __init__ import db
-
-class Book(db.Model):
-    id = db.Column('bookId', db.Integer, primary_key = True)
-    bookName = db.Column(db.String(255))
-    bookStatus = db.Column(db.String(255))
-    def __repr__(self):
-        return '<Book {}>'.format(self.bookName)
