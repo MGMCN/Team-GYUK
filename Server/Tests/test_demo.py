b055773f90ee00ad7c61ee10a211c1c25c05e7ad
Takayama <49364055+MGMCN@users.noreply.github.com>
2023-02-06 12:57:25 +0900

    add test_demo.py

---
+++ Server/Tests/test_demo.py
@@ -0,0 +1,24 @@
+"""
+    How to create test file
+    filename should be like test_xxx.py
+
+    How to test
+    step1 : Make sure you download pytest
+    step2 : run "pytest" command
+"""
+import pytest
+import sys
+
+sys.path.insert(0, '../Server')
+from app import app
+
+
+@pytest.fixture
+def client():
+    return app.test_client()
+
+
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Hello World!' in response.data
