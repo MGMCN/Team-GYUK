a829e28182f5ef4d7890fa97f737add15708b090
WEI44ZHEYUAN <yuenwei44@gmail.com>
2023-02-11 06:50:24 +0000

    Update some testcase

--- Server/Tests/test_demo.py
+++ Server/Tests/test_demo.py
@@ -13,3 +13,24 @@ def test_hello_world():
     response = requests.request(url='http://127.0.0.1:8883/', method='GET')
     assert response.status_code == 200
     assert "Hello World!" == response.text
+
+
+def test_get_booklist():
+    reponse = requests.request(url= 'http://127.0.0.1:8883/getbookslist', method='GET')
+    assert reponse.status_code == 200
+
+def test_add_books():
+    reponse = requests.request(url= 'http://127.0.0.1:8883/addbooks', method='POST')
+    assert reponse.status_code == 200
+
+def test_delete_books():
+    reponse = requests.request(url= 'http://127.0.0.1:8883/deletebooks', method='POST')
+    assert reponse.status_code == 200
+
+def test_borrow_books():
+    reponse = requests.request(url='http://127.0.0.1:8883/borrowbooks', method= 'POST')
+    assert reponse.status_code == 200
+
+def test_return_books():
+    reponse = requests.request(url='http://127.0.0.1:8883/returnbooks', method='POST')
+    assert reponse.status_code == 200
