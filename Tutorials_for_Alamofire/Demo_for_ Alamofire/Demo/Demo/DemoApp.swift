2c4e43f7c7be4af04112cb3db62b6fe9823561ea
Takayama <49364055+MGMCN@users.noreply.github.com>
2023-02-15 00:41:04 +0900

    add Tutorials for Alamofire

---
+++ Tutorials_for_Alamofire/Demo_for_ Alamofire/Demo/Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by GS on 2023/2/15.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
