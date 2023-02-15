44be87e49c23fb03e08743bd6d6fc8e74031a28a
SK_Kazuki <sendakazuki6@gmail.com>
2023-02-15 19:31:03 +0900

    #12 feat: add Book API (getbooklist, addbooks, deletebooks, borrowbooks, returnbooks)

--- Server/bksf.py
+++ Server/bksf.py
@@ -9,30 +9,13 @@ bksfbp = Blueprint('bksfbp',__name__) #,url_prefix='/books')
 def show_all():
    cs = mysql.connection.cursor()
    cs.execute('''SELECT * FROM book''')
-   result = json.dumps(cs.fetchall())
+   result = str(cs.fetchall())
    cs.close()
    return result
 
-@bksfbp.route('/addbooks', methods=['POST'])
-def register():
-    errors = validate_form_data(request.form)
-    if errors:
-        return errors, 500
-    try:
-        cursor = mysql.connection.cursor()
-        sql = """INSERT INTO book (bookName, bookStatus) VALUES (%s, %s)"""
-        cursor.execute(sql, extract_form_data(request.form))
-        mysql.connection.commit()
-    except Exception as e:
-        return str(e), 500
-
-    return  "Success", 200
-
-def headers():
-    return ['bookName', 'bookStatus']
-def validate_form_data(form):
+def validate_form_data(form, headers):
     errors = []
-    for header in headers():
+    for header in headers:
         if not form[header]:
             errors.append(f"{header} is empty")
     return errors
@@ -41,36 +24,77 @@ def row_count():
     cursor.execute("SELECT COUNT(*) FROM book")
     count = cursor.fetchone()[0] # to get current ID
     return count + 1
-def extract_form_data(form):
+def extract_form_data(form, headers):
     values = []
     # values.append(row_count())
-    for header in headers():
+    for header in headers:
         values.append(form[header])
     return values
 
-@bksfbp.route('/<int:id>/update', methods=['POST'])
-def update(id):
-    errors = validate_form_data(request.form)
+def add_headers():
+    return ['bookName']
+@bksfbp.route('/addbooks', methods=['POST'])
+def register():
+    errors = validate_form_data(request.form, add_headers())
     if errors:
         return errors, 500
     try:
         cursor = mysql.connection.cursor()
-        sql = """UPDATE book SET bookName=%s, bookStatus=%s WHERE bookId=%s"""
-        cursor.execute(sql, extract_form_data(request.form) + [id])
+        sql = """INSERT INTO book (bookName, bookStatus) VALUES (%s, %s)"""
+        cursor.execute(sql, extract_form_data(request.form, add_headers()) + ['available'])
         mysql.connection.commit()
     except Exception as e:
         return str(e), 500
 
     return  "Success", 200
 
+def update_headers():
+    return ['bookId']
 @bksfbp.route('/deletebooks', methods=['POST'])
 def delete():
-    if not request.form['bookId']:
-        return "bookId is empty", 500
+    errors = validate_form_data(request.form, update_headers())
+    if errors:
+        return errors, 500
     try:
         cursor = mysql.connection.cursor()
         sql = """DELETE FROM book WHERE bookId=%s"""
-        cursor.execute(sql, [request.form['bookId']])
+        cursor.execute(sql, extract_form_data(request.form, update_headers()))
+        mysql.connection.commit()
+    except Exception as e:
+        return str(e), 500
+
+    return  "Success", 200
+
+@bksfbp.route('/returnbooks', methods=['POST'])
+def returnbooks():
+    errors = validate_form_data(request.form, update_headers())
+    if errors:
+        return errors, 500
+    try:
+        cursor = mysql.connection.cursor()
+        sql = """UPDATE book SET bookStatus=%s WHERE bookId=%s"""
+        cursor.execute(sql, ['available'] + extract_form_data(request.form, update_headers()))
+        sql = """DELETE FROM borrowStatus WHERE bookId = %s"""
+        cursor.execute(sql, extract_form_data(request.form, update_headers()))
+        mysql.connection.commit()
+    except Exception as e:
+        return str(e), 500
+
+    return  "Success", 200
+
+def borrow_headers():
+    return ['bookId', 'userId']
+@bksfbp.route('/borrowbooks', methods=['POST'])
+def borrowbooks():
+    errors = validate_form_data(request.form, borrow_headers())
+    if errors:
+        return errors, 500
+    try:
+        cursor = mysql.connection.cursor()
+        sql = """UPDATE book SET bookStatus=%s WHERE bookId=%s"""
+        cursor.execute(sql, ['renting', request.form['bookId']])
+        sql = """INSERT INTO borrowStatus (bookId, userId) VALUES (%s, %s)"""
+        cursor.execute(sql, extract_form_data(request.form, borrow_headers()))
         mysql.connection.commit()
     except Exception as e:
         return str(e), 500
@@ -78,6 +102,21 @@ def delete():
     return  "Success", 200
 
 """
+# RestfulAPI
+@bksfbp.route('/<int:id>/update', methods=['POST'])
+def update(id):
+    errors = validate_form_data(request.form, headers())
+    if errors:
+        return errors, 500
+    try:
+        cursor = mysql.connection.cursor()
+        # sql = UPDATE book SET bookName=%s, bookStatus=%s WHERE bookId=%s
+        cursor.execute(sql, extract_form_data(request.form, headers()) + [id])
+        mysql.connection.commit()
+    except Exception as e:
+        return str(e), 500
+
+    return  "Success", 200
 @bksfbp.route('/<int:id>/delete', methods=['POST'])
 def delete(id):
     try:
