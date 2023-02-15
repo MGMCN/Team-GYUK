9d06010ddccfc953eea03b3ccf42e41e0092264e
Takayama <49364055+MGMCN@users.noreply.github.com>
2023-02-15 23:15:06 +0900

    do swiftformat

--- Client/Client/account.swift
+++ Client/Client/account.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-class account{
-    static var username :String = "username"
-    static var email:String? = "email"
-    static var password:String? = "password"
+class account {
+    static var username: String = "username"
+    static var email: String? = "email"
+    static var password: String? = "password"
     init() {}
-    static func set(username : String,email: String,password:String){
+    static func set(username: String, email: String, password: String) {
         self.username = username
         self.email = email
         self.password = password
