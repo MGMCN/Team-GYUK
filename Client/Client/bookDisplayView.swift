609e3023e58b5fbf01b868f1fb2bf3d49759b3eb
Takayama <49364055+MGMCN@users.noreply.github.com>
2023-02-18 17:07:45 +0900

    do swiftformat

--- Client/Client/bookDisplayView.swift
+++ Client/Client/bookDisplayView.swift
@@ -14,7 +14,7 @@ struct bookDisplayView: View {
         "book1", "book2", "book3", "book4", "book5", "book6", "book7", "book8",
         "book9", "book10",
     ]
-    
+
     @State var bookStates: [String] = []
 
     var body: some View {
